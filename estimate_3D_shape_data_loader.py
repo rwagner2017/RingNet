bbe462c3e656c784f2a7a045400c17c7b347a13d
Robert Wagner <rwagner@visionsystemsinc.com>
2020-03-05 16:30:15 -0500

    added file saving

--- estimate_3D_shape_data_loader.py
+++ estimate_3D_shape_data_loader.py
@@ -23,70 +23,71 @@ IMG_WIDTH = 224
 
 
 def decode_img(img):
-  # convert the compressed string to a 3D uint8 tensor
-  img = tf.image.decode_jpeg(img, channels=3)
-  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-  img = tf.image.convert_image_dtype(img, tf.float32)
-  # resize the image to the desired size.
-  img = tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize the image to the desired size.
+    img = tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
 
-  # Normalize image to [-1, 1]
-  img = tf.math.subtract(img, -0.5)
-  img = tf.math.multiply(img, 2)
+    # Normalize image to [-1, 1]
+    img = tf.math.subtract(img, -0.5)
+    img = tf.math.multiply(img, 2)
 
-  return img
+    return img
 
 
 def process_path(file_path):
-  # label = get_label(file_path)
-  label = 0
-  # load the raw data from the file as a string
-  img = tf.io.read_file(file_path)
-  img = decode_img(img)
+    # label = get_label(file_path)
+    label = 0
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
 
-  return img, label
+    return img, label, file_path
 
 
 def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
-  # This is a small dataset, only load it once, and keep it in memory.
-  # use `.cache(filename)` to cache preprocessing work for datasets that don't
-  # fit in memory.
-  if cache:
-    if isinstance(cache, str):
-      ds = ds.cache(cache)
-    else:
-      ds = ds.cache()
+    # This is a small dataset, only load it once, and keep it in memory.
+    # use `.cache(filename)` to cache preprocessing work for datasets that don't
+    # fit in memory.
+    if cache:
+        if isinstance(cache, str):
+            ds = ds.cache(cache)
+        else:
+            ds = ds.cache()
 
-  ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
 
-  # Repeat forever
-  ds = ds.repeat()
+    # Repeat forever
+    ds = ds.repeat()
 
-  ds = ds.batch(BATCH_SIZE)
+    ds = ds.batch(BATCH_SIZE)
 
-  # `prefetch` lets the dataset fetch batches in the background while the model
-  # is training.
-  ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    # `prefetch` lets the dataset fetch batches in the background while the model
+    # is training.
+    ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-  return ds
+    return ds
+
 
 def prepare_for_testing(ds, cache=True):
-  # This is a small dataset, only load it once, and keep it in memory.
-  # use `.cache(filename)` to cache preprocessing work for datasets that don't
-  # fit in memory.
-  if cache:
-    if isinstance(cache, str):
-      ds = ds.cache(cache)
-    else:
-      ds = ds.cache()
+    # This is a small dataset, only load it once, and keep it in memory.
+    # use `.cache(filename)` to cache preprocessing work for datasets that don't
+    # fit in memory.
+    if cache:
+        if isinstance(cache, str):
+            ds = ds.cache(cache)
+        else:
+            ds = ds.cache()
 
-  ds = ds.batch(BATCH_SIZE)
+    ds = ds.batch(BATCH_SIZE)
 
-  # `prefetch` lets the dataset fetch batches in the background while the model
-  # is training.
-  ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    # `prefetch` lets the dataset fetch batches in the background while the model
+    # is training.
+    ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-  return ds
+    return ds
 
 
 def preprocess_image_224(img_path):
@@ -96,34 +97,44 @@ def preprocess_image_224(img_path):
 import time
 default_timeit_steps = 1000
 
-def timeit(ds, steps=default_timeit_steps):
-  start = time.time()
-  it = iter(ds)
-  for i in range(steps):
-    batch = next(it)
-    if i%10 == 0:
-      print('.',end='')
-  print()
-  end = time.time()
 
-  duration = end-start
-  print("{} batches: {} s".format(steps, duration))
-  print("{:0.5f} Images/s".format(BATCH_SIZE*steps/duration))
+def timeit(ds, steps=default_timeit_steps):
+    start = time.time()
+    it = iter(ds)
+    for i in range(steps):
+        batch = next(it)
+        if i % 10 == 0:
+            print('.', end='')
+    print()
+    end = time.time()
+
+    duration = end - start
+    print("{} batches: {} s".format(steps, duration))
+    print("{:0.5f} Images/s".format(BATCH_SIZE * steps / duration))
 
 
 def main(config):
+    sys.path.insert(0, os.path.join(os.getcwd(), 'RingNet'))
+    print('PYTHONPATH')
+    for path in sys.path:
+        print(path)
+    template_mesh = Mesh(filename='./flame_model/FLAME_sample.ply')
+
     print('Tensorflow version {}'.format(tf.__version__))
 
     print("Input Dir: <{}>".format(config.in_folder))
     print("Output Dir: <{}>".format(config.out_folder))
 
-    img_paths = glob.glob(os.path.join(config.in_folder, '*/*'))
-    n_images = len(img_paths)
+    globbed_img_paths = glob.glob(os.path.join(config.in_folder, '*/*'))
+    n_images = len(globbed_img_paths)
 
-    for img_path in img_paths:
-        print(img_path)
+
+    # print('img_paths')
+    # for img_path in img_paths:
+    #     print(img_path)
 
     list_ds = tf.data.Dataset.list_files(os.path.join(config.in_folder, '*/*'))
+    # print('list_ds')
     # for f in list_ds.take(5):
     #     print(f.numpy())
 
@@ -145,24 +156,51 @@ def main(config):
     pre_process_times = []
     inference_times = []
 
+    n_reps = int(np.floor(n_images/BATCH_SIZE))
+    if (n_images % BATCH_SIZE != 0 or n_reps == 0):
+        n_reps += 1
+    print('n_images {}, batch size {}, n_reps = {}'.format(n_images, BATCH_SIZE, n_reps))
 
-    for idx in range(n_images):
-        start = time.time()
+    for batch_num in range(n_reps):
         next_element = iterator.get_next()
-        input_img = next_element[0].eval(session=model.sess)[0]
-        end = time.time()
-        duration = end - start
-        pre_process_times.append(duration)
+        batch = next_element[0].eval(session=model.sess)
+        img_paths = next_element[2].eval(session=model.sess)
+        print(img_paths.shape)
+        print(type(img_paths))
+        print(type(img_paths[0]))
+        print(img_paths)
+        n_batch_images = batch.shape[0]
+        print('{}: {}'.format(batch_num, n_batch_images))
+        print('batch number {}'.format(batch_num))
+        for idx in range(n_batch_images):
+            img_path = img_paths[idx].decode('UTF-8')
+            print('{}: <{}>'.format(idx, img_path))
+            img_name = img_path.split('/')[-1][:-4]
 
+            start = time.time()
+            input_img = batch[idx]
+            end = time.time()
+            duration = end - start
+            pre_process_times.append(duration)
 
-        start = time.time()
-        vertices, flame_parameters = model.predict(np.expand_dims(input_img, axis=0), get_parameters=True)
-        end = time.time()
-        duration = end - start
-        inference_times.append(duration)
+            start = time.time()
+            vertices, flame_parameters = model.predict(np.expand_dims(input_img, axis=0), get_parameters=True)
+            end = time.time()
+            duration = end - start
+            inference_times.append(duration)
 
-        # print(flame_parameters)
+            if config.save_obj_file:
+                if not os.path.exists(config.out_folder + '/mesh'):
+                    os.mkdir(config.out_folder + '/mesh')
+                mesh = Mesh(v=vertices[0], f=template_mesh.f)
+                mesh.write_obj(config.out_folder + '/mesh/' +  '{}.obj'.format(img_name))
 
+            if config.save_flame_parameters:
+                if not os.path.exists(config.out_folder + '/params'):
+                    os.mkdir(config.out_folder + '/params')
+                flame_parameters_ = {'cam':  flame_parameters[0][:3], 'pose': flame_parameters[0][3:3+config.pose_params], 'shape': flame_parameters[0][3+config.pose_params:3+config.pose_params+config.shape_params],
+                 'expression': flame_parameters[0][3+config.pose_params+config.shape_params:]}
+                np.save(config.out_folder + '/params/' + '{}.npy'.format(img_name), flame_parameters_)
     pass
 
     mean_pre_process_time = np.mean(pre_process_times)
@@ -175,50 +213,6 @@ def main(config):
     throughput = n_images / np.sum(pre_process_times) + np.sum(inference_times)
     print('total images = {} throughput = {}/s'.format(n_images, throughput))
 
-    # for img_path in img_paths:
-    #     sess = tf.Session()
-    #     model = RingNet_inference(config, sess=sess)
-    #     input_img, proc_param, img = preprocess_image(img_path, config.img_size)
-    #
-    #     # vertices, flame_parameters = model.predict(np.expand_dims(input_img, axis=0), get_parameters=True)
-    #     # print('vertices shape = {}'.format(vertices.shape))
-    #     # print('flame_parameters shape = {}'.format(flame_parameters.shape))
-    #
-    #     results = model.predict_dict([input_img, input_img])
-    #     print(results)
-    #     vertices = results['vertices']
-    #     flame_parameters = results['parameters']
-    #     print('dict vertices shape = {}'.format(vertices.shape))
-    #     print('dict flame_parameters shape = {}'.format(flame_parameters.shape))
-    #
-    #     if config.save_viz:
-    #         if not os.path.exists(config.out_folder + '/images'):
-    #             os.mkdir(config.out_folder + '/images')
-    #
-    #         cams = flame_parameters[0][:3]
-    #         template_mesh = Mesh(filename='./flame_model/FLAME_sample.ply')
-    #         renderer = vis_util.SMPLRenderer(faces=template_mesh.f)
-    #         visualize(img, proc_param, vertices[0], cams, renderer, img_name=config.out_folder + '/images/' + img_path.split('/')[-1][:-4])
-    #
-    #     if config.save_obj_file:
-    #         if not os.path.exists(config.out_folder + '/mesh'):
-    #             os.mkdir(config.out_folder + '/mesh')
-    #         mesh = Mesh(v=vertices[0], f=template_mesh.f)
-    #         mesh.write_obj(config.out_folder + '/mesh/' + img_path.split('/')[-1][:-4] + '.obj')
-    #
-    #     if config.save_flame_parameters:
-    #         if not os.path.exists(config.out_folder + '/params'):
-    #             os.mkdir(config.out_folder + '/params')
-    #         flame_parameters_ = {'cam':  flame_parameters[0][:3], 'pose': flame_parameters[0][3:3+config.pose_params], 'shape': flame_parameters[0][3+config.pose_params:3+config.pose_params+config.shape_params],
-    #          'expression': flame_parameters[0][3+config.pose_params+config.shape_params:]}
-    #         np.save(config.out_folder + '/params/' + img_path.split('/')[-1][:-4] + '.npy', flame_parameters_)
-    #
-    #     if config.neutralize_expression:
-    #         from util.using_flame_parameters import make_prdicted_mesh_neutral
-    #         if not os.path.exists(config.out_folder + '/neutral_mesh'):
-    #             os.mkdir(config.out_folder + '/neutral_mesh')
-    #         neutral_mesh = make_prdicted_mesh_neutral(config.out_folder + '/params/' + img_path.split('/')[-1][:-4] + '.npy', config.flame_model_path)
-    #         neutral_mesh.write_obj(config.out_folder + '/neutral_mesh/' + img_path.split('/')[-1][:-4] + '.obj')
 
 
 if __name__ == '__main__':
